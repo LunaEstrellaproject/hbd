354816076a188fc2cb9d40990f8610004e2a33f2
Laysee <68098987+LunaEstrellaproject@users.noreply.github.com>
2026-01-17 14:51:09 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -192,8 +192,8 @@ const animationTimeline = () => {
         0.5, {
             scale: 3.5,
             opacity: 0,
-            x: 70,
-            y: 1000,
+            x: 25,
+            y: -25,
             rotationZ: -45,
         },
         "-=2"
